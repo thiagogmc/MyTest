09ad00212850363c0085160a67ce60d487256c3b
Thiago Costa <thgmcosta@gmail.com>
2018-08-05 14:46:55 -0300

    Add vue resource

--- myTest/src/main.js
+++ myTest/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import { routes } from './routes';
+import VueResource from 'vue-resource';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const router = new VueRouter({
   routes,
